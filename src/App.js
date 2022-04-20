3155ea6b1615aa7b4a4f96d2a0a506e53d9ddbf4
exploit-j <kkarismajh@gmail.com>
2022-04-20 13:05:27 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import {Navbar, Nav, Container} from 'react-bootstrap'
+// import './styles/main.scss'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MainNav></MainNav>
+      <div className='container'>
+        <h1 className='h1'>
+          sass test
+        </h1>
+      </div>
     </div>
   );
+}
+
+function MainNav(){
+  return(
+    <Navbar bg="light" variant="light">
+      <Container>
+      <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+      <Nav className="me-auto">
+        <Nav.Link href="#home">Home</Nav.Link>
+        <Nav.Link href="#features">Features</Nav.Link>
+        <Nav.Link href="#pricing">Pricing</Nav.Link>
+      </Nav>
+      </Container>
+    </Navbar>
+  )
 }
 
 export default App;
